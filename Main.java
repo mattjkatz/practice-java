0868dbb633727769cd66438ec933cf49a98ad243
mattjkatz <matthewkatz7@gmail.com>
2022-07-28 21:15:25 -0500

    omg it works i want to cry

--- Main.java
+++ Main.java
@@ -1,8 +1,24 @@
+import java.util.Arrays;
+
 public class Main {
-  public static void main(String[] names) {
-    int[] numbers = { 3, 2, 5, 7, 9, 6, 1 };
-    var sorter = new BubbleSort();
-    sorter.sort(numbers);
-    System.out.println(numbers);
+  public static void main(String[] args) {
+    // System.out.println("Hello World - this is a test");
+    int[] arr = new int[5];
+    arr[0] = 4;
+    arr[1] = 2;
+    arr[2] = 6;
+    arr[3] = 9;
+    arr[4] = 3;
+    int n = arr.length;
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = 0; j < n - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          int temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+    System.out.println(Arrays.toString(arr));
   }
 }
